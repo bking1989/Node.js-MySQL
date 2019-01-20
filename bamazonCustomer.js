d079afd5698c8b99b5f55bb6832f6c1eb56a2182
bking1989 <bradley_king@me.com>
2019-01-19 23:39:17 -0700

    Completed the first part of the assignment and updated SQL file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,33 +11,86 @@ const connection = mysql.createConnection({
     database : "bamazon_db"
 });
 
-// Function for formatting database information
-const outputFormat = (res) => {
-    res.forEach(function(item) {
-        console.log(`[ Item # ${item.item_id} - ${item.product_name} - $${item.price} - ${item.stock_quantity} available ]\n`);
-    });
-};
-
 // Function for producing a list of the products
 const inventory = () => {
     connection.query('SELECT * FROM bamazon_db.products', function(err, res, fields) {
         if (err) throw err;
         console.table(res);
     });
-
-    connection.end();
 };
 
-// Inquirer function for customers
-const customerMenu = () => {
+// Function for updating the store database
+const storeUpdate = (itemID, quantity) => {
+    connection.query('UPDATE bamazon_db.products SET stock_quantity = stock_quantity - ? WHERE item_id = ?', [quantity, itemID], function(err, res, fields) {
+        if (err) throw err;
+    });
+};
+
+// Function for checking if an item is in stock
+const checkInventory = (itemID, quantity) => {
+    connection.query('SELECT * FROM `products` WHERE `item_id` = ?', itemID, function(err, res, fields) {
+        if (err) throw err;
+
+        if (quantity <= res[0].stock_quantity) {
+            var customerTotal = quantity * res[0].price;
+
+            console.log(`\nYour order for ${quantity} ${res[0].product_name} comes to a total of $${customerTotal}\n`);
+
+            storeUpdate(itemID, quantity);
+
+            console.log("\nYour order will be placed right away. Thank you for shopping with us!\n");
+
+            connection.end();
+        } else if (quantity > res[0].stock_quantity) {
+            console.log("\nI'm sorry, but we don't have enough of this item in stock! Please try a different order.\n");
+            customerMenu1();
+        };
+    });
+};
+
+// Inquirer function ( with variables) for customers
+var customerItem = "";
+var customerQuantity = "";
+
+const customerMenu1 = () => {
     inquirer.prompt([
         {
-            name: "customerMenu",
+            name: "customerMenu1",
             message: "Please enter the ID number for the item you wish to purchase.",
             type: "input"
         }
     ]).then((answer) => {
-        console.log(answer);
+        customerItem = answer.customerMenu1;
+        customerMenu2();
+    });
+};
+
+const customerMenu2 = () => {
+    inquirer.prompt([
+        {
+            name: "customerMenu2",
+            message: "And how many of this item would you like?",
+            type: "input"
+        }
+    ]).then((answer) => {
+        customerQuantity = answer.customerMenu2;
+        customerOrder();
+    });
+};
+
+const customerOrder = () => {
+    inquirer.prompt([
+        {
+            name: "customerConfirm",
+            message: `Your order is for ${customerQuantity} of item # ${customerItem}. Is this correct?`,
+            type: "confirm"
+        }
+    ]).then((answer) => {
+        if (answer.customerConfirm === true) {
+            checkInventory(customerItem, customerQuantity);
+        } else if (answer.customerConfirm === false) {
+            customerMenu1();
+        }
     });
 };
 
@@ -46,7 +99,7 @@ const bamazonStore = () => {
     console.log("\n");
     console.log(`\nHello and welcome to Bamazon! Here is what we have for sale, today:\n`)
     inventory();
-    customerMenu();
+    customerMenu1();
 };
 
 bamazonStore();
